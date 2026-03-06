50b73340df133d2cf0e7042d9fb2aaec6f831164
Larissa Bitencourt <larissabitencourt95@gmail.com>
2026-03-06 15:44:54 -0300

    feat: variáveis de ambiente e conexão mongoDB

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Conectado ao MongoDB com sucesso! ✅'))
+  .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+
 app.get('/', (req, res) => {
-  res.send('API Jitterbit Online! 🚀');
+  res.send('API Jitterbit com MOngoDB conectada! 🚀');
 });
 
 const PORT = 3000;
