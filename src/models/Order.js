eab5eb983908d5142feb1b707df3d4f776180597
Larissa Bitencourt <larissabitencourt95@gmail.com>
2026-03-06 17:14:12 -0300

    feat: criação dos modelos de Usuário e Pedido

---
+++ src/models/Order.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+  orderId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  value: { 
+    type: Number, 
+    required: true 
+  },
+  creationDate: { 
+    type: Date, 
+    required: true 
+  },
+  items: [
+    {
+      productId: { type: Number, required: true },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true }
+    }
+  ]
+}, { 
+  versionKey: false
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
