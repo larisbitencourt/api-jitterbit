97b1cb0327917c121ee642d0fcac2379e306a241
Larissa Bitencourt <larissabitencourt95@gmail.com>
2026-03-07 08:43:40 -0300

    feat: refatoração do UserSchema para incluir níveis de acesso

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,16 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, select: false }
+  password: { type: String, required: true, select: false },
+  role: { 
+    type: String, 
+    enum: ['admin', 'sid', 'user'], 
+    default: 'user' 
+  },
+  holly: { 
+    type: String, 
+    default: null 
+  }
 }, { 
   timestamps: true,
   versionKey: false
