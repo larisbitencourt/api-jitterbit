a846e1af5224a838f4d0447f2ff032383bf83e84
Larissa Bitencourt <larissabitencourt95@gmail.com>
2026-03-08 12:51:44 -0300

    'chore: implementação dos middlewares na rota /user'

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const UserController = require('../controllers/UserController');
 const AuthController = require('../controllers/AuthController');
+const isAdmin = require('../middlewares/isAdmin');
+const authMiddleware = require('../middlewares/auth');
 
 const userRoutes = new Router();
 
 userRoutes.post('/login', AuthController.login);
-userRoutes.post('/user', UserController.store);
+userRoutes.post('/user', authMiddleware, isAdmin, UserController.store);
 
 module.exports = userRoutes;
