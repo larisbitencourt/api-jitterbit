527f8892cf84ebb0a7a6957faac9df30ee0b089c
Larissa Bitencourt <larissabitencourt95@gmail.com>
2026-03-08 01:40:36 -0300

    'chore: implementação do swagger'

--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./config/swagger.json');
+
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(routes);
 
 app.get('/', (req, res) => {
